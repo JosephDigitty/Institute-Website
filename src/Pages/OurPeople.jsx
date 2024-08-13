7dccd2d0edd325e718d42816a1b90f9ceb642f7e
Joseph Anamene <joseph.anamene@c-ileasing.com>
2024-08-13 15:24:49 +0100

    Initial commit

--- src/Pages/OurPeople.jsx
+++ src/Pages/OurPeople.jsx
@@ -1,8 +1,137 @@
 import React from 'react'
+import mike from '../assets/images/Mike.jpg'
+import adam from '../assets/images/Adam.jpg'
+import Akin from '../assets/images/Akinrinade.jpg'
+import Elley from '../assets/images/Elley.jpg'
+import Gal from '../assets/images/Galadinma.jpg'
+import issa from '../assets/images/Issa.jpg'
+import joy from '../assets/images/joy.jpg'
+import muhammed from '../assets/images/muhammed.jpg'
+import mustapha from '../assets/images/mustapha.jpg'
+import oye from '../assets/images/Oyewole.jpg'
+import Steve from '../assets/images/Stephen.jpg'
+import udeh from '../assets/images/Udeh.jpg'
+
 
 const OurPeople = () => {
   return (
-    <div>OurPeople</div>
+    <section className='bg-gray-300'>
+        <div className='md:px-[280px] pb-4 px-7'>
+          <h2 className='py-10 md:text-3xl text-center sm:taxt-2xl text-xl font-ptserif text-slate-700 hover:text-zinc-700 font-semibold '>
+            Our Govering council
+          </h2>
+          <div className='grid lg:grid-cols-3 gap-10 py-8 md:grid-cols-2 px-16 grid-cols-1'>
+            <div className='pt-4 px-3  bg-white/80 rounded-lg '>
+              <img src={mike} alt="" className='w-full rounded-2xl h-80 object-contain' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              National President/Chairman Governing Council
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Prof. Mike Ikupolati,  <span className='text-sm italic font-HR'>FCPDM</span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={oye} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Registrar/Secretary Governing Council
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Dr. Oyewole Sarumi,  <span className='text-sm italic font-HR'>FCPDM</span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={Steve} alt="" className='w-full rounded-2xl h-96 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Executive Director
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Stephen Odukoya,  <span className='text-sm italic font-HR'>FCPDM</span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={issa} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Abuja
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Isa Wasagu Muhammed-Nasir <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={Akin} alt="" className='w-full rounded-2xl h-96 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Lagos
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Akinaride Morakinyo O. <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={udeh} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Abuja
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Udeh Augustine Nkem  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={mustapha} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Kwara State
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Mustapha Rahman Olademiji  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={joy} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Abuja
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Obiogbe Joy Aina  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={adam} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Borno State
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Hayatudeen Mohammed Adam  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={muhammed} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Abuja
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Muhammed Salihu Olowo  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={Elley} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Rivers State
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Elleh Oyoboy Edman  <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+            <div className='pt-4 px-3 bg-white/80 rounded-lg '>
+              <img src={Gal} alt="" className='w-full rounded-2xl h-80 object-cover ' />
+              <p className='italic text-sm font-ptserif pt-3 text-red-400 '>
+              Governing Council, Member Abuja
+              </p>
+              <p className='font-semibold text-xl font-ptserif py-3' >
+              Galadinma Daniel Habila <span className='text-sm italic font-HR'></span>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
 
--- src/Pages/Upgrades.jsx
+++ src/Pages/Upgrades.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Upgrades = () => {
   return (
-    <div>Upgrades</div>
+    <section className='md:px-[395px] px-10 pb-6 bg-gray-200 py-3 '>
+      <h2 className='text-sm font-ptserif text-slate-600 font-semibold py-4 '>
+        Membership
+      </h2>
+      <h3 className='text-2xl font-ptserif text-slate-600 font-semibold py-4 '>
+        Membership Upgrades
+      </h3>
+      <h4 className='text-lg font-ptserif text-slate-600 font-semibold pb-2 '>
+      Upgrade criteria 
+      </h4>
+      <p className='leading-loose font-Merriweather text-black/85'>
+      1- members must have been inducted <br /> 
+      2- At least such inducted member must have been in the institute for a period of not less than 2 years <br />
+      3- He/she must have attended at least 3 training ( MPTP) to be eligible for upgrade <br />
+      4- Annual Subscription must have been paid till date. <br />
+      5- Contributions to his/her local unit of the institute is acceptable <br />
+      6- Other relevant pre- requisite will be required for higher membership status <br />
+      </p>
+      <h4 className='text-lg font-ptserif text-slate-600 font-semibold pt-3 pb-2' >
+      Upgrade Procedures
+      </h4>
+      <p className='leading-loose font-Merriweather pb-8 text-black/85'>
+      1- Members are to obtain the institute upgrade form soft copy/ hardcopy <br/>
+      2- Furnish the information required thoroughly <br/>
+      3- Send filled upgrade form to the  membership service department at the head office via internet or local coordinator <br/>
+      4- A letter of Upgrade Approval will be issued if such member is qualified
+      </p>
+    </section>
   )
 }
 
